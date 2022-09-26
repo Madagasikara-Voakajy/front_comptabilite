9347bb11683c58eb438bb28a5084d5e55fa8716d
San randry <sinrandry@gmail.com>
2022-09-26 16:57:05 +0300

    review code

--- pages/grand-livre/toutes-les-operations/index.tsx
+++ pages/grand-livre/toutes-les-operations/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-<<<<<<< pages/grand-livre/toutes-les-operations/index.tsx
-import BackOfficeLayout from "../../../layouts/backOffice";
-import { Container } from "@mui/material";
-=======
 import { Container } from "@mui/material";
 import BackOfficeLayout from "../../../layouts/backOffice";
->>>>>>> pages/grand-livre/toutes-les-operations/index.tsx
 import ToutesLesOperations from "../../../components/grand-livre/grandLivre/toutesLesOperation";
 
 const Index = () => {
