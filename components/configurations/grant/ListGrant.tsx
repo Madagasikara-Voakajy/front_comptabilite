e9c0f6a243477ab56e13b1a6966857792b20073f
Paul Odon <46904458+PaulOdon@users.noreply.github.com>
2022-10-06 22:49:28 +0300

    fix container maxwith, in home and in grant page

--- components/configurations/grant/ListGrant.tsx
+++ components/configurations/grant/ListGrant.tsx
@@ -1,10 +1,10 @@
 import {
-  Button,
-  Container,
-  IconButton,
-  Stack,
-  styled,
-  Typography,
+	Button,
+	Container,
+	IconButton,
+	Stack,
+	styled,
+	Typography,
 } from "@mui/material";
 import Link from "next/link";
 import React, { useEffect } from "react";
@@ -16,7 +16,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
+import Divider from "@mui/material/Divider";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -32,10 +32,10 @@ import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 //   updateAuxiliairyAccount,
 // } from "../../../redux/features/auxiliairyAccount";
 import {
-  defaultLabelDisplayedRows,
-  getComparator,
-  labelRowsPerPage,
-  Order,
+	defaultLabelDisplayedRows,
+	getComparator,
+	labelRowsPerPage,
+	Order,
 } from "../../../config/table.config";
 import { useRouter } from "next/router";
 import useFetchGrants from "./hooks/useFetchGrants";
@@ -46,234 +46,245 @@ import GrantTableToolbar from "./table/GrantTableToolbar";
 // import { AuxiliairyAccountItem } from "../../../redux/features/auxiliairyAccount/auxiliairyAccount.interface";
 
 const ListGrant = () => {
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const dispatch: any = useAppDispatch();
-  const listGrants: any = [];
-  // const { listGrants } = useAppSelector(
-  //   (state) => state.auxiliaryAccount
-  // );
-  const router = useRouter();
-  const confirm = useConfirm();
+	const [page, setPage] = React.useState(0);
+	const [dense, setDense] = React.useState(false);
+	const [rowsPerPage, setRowsPerPage] = React.useState(5);
+	const dispatch: any = useAppDispatch();
+	const listGrants: any = [];
+	// const { listGrants } = useAppSelector(
+	//   (state) => state.auxiliaryAccount
+	// );
+	const router = useRouter();
+	const confirm = useConfirm();
 
-  const fetchGrantList = useFetchGrants();
+	const fetchGrantList = useFetchGrants();
 
-  useEffect(() => {
-    fetchGrantList();
-  }, [router.query]);
+	useEffect(() => {
+		fetchGrantList();
+	}, [router.query]);
 
-  const handleClickEdit = async (id: any) => {
-    // await dispatch(editAuxiliairyAccount({ id }));
-  };
+	const handleClickEdit = async (id: any) => {
+		// await dispatch(editAuxiliairyAccount({ id }));
+	};
 
-  const handleclickDelete = async (id: any) => {
-    confirm({
-      title: "Supprimer ce grant ",
-      description: "Voulez-vous vraiment supprimer ce grant ?",
-      cancellationText: "Annuler",
-      confirmationText: "Supprimer",
-      cancellationButtonProps: {
-        color: "warning",
-      },
-      confirmationButtonProps: {
-        color: "error",
-      },
-    })
-      .then(async () => {
-        // await dispatch(deleteAuxiliairyAccount({ id }));
-        fetchGrantList();
-      })
-      .catch(() => {});
-  };
+	const handleclickDelete = async (id: any) => {
+		confirm({
+			title: "Supprimer ce grant ",
+			description: "Voulez-vous vraiment supprimer ce grant ?",
+			cancellationText: "Annuler",
+			confirmationText: "Supprimer",
+			cancellationButtonProps: {
+				color: "warning",
+			},
+			confirmationButtonProps: {
+				color: "error",
+			},
+		})
+			.then(async () => {
+				// await dispatch(deleteAuxiliairyAccount({ id }));
+				fetchGrantList();
+			})
+			.catch(() => {});
+	};
 
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
+	const handleChangePage = (event: unknown, newPage: number) => {
+		setPage(newPage);
+	};
 
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+	const handleChangeRowsPerPage = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
 
-  // function getColorStatus(status: any) {
-  //   switch (status) {
-  //     case "PENDING":
-  //       return "info";
-  //     case "APPROVED":
-  //       return "primary";
-  //     case "REJECTED":
-  //       return "warning";
-  //     default:
-  //       break;
-  //   }
-  // }
-  // function getTextStatus(status: any) {
-  //   switch (status) {
-  //     case "PENDING":
-  //       return "En_attente";
-  //     case "APPROVED":
-  //       return "Validé";
-  //     case "REJECTED":
-  //       return "Refusé";
-  //     default:
-  //       break;
-  //   }
-  // }
+	// function getColorStatus(status: any) {
+	//   switch (status) {
+	//     case "PENDING":
+	//       return "info";
+	//     case "APPROVED":
+	//       return "primary";
+	//     case "REJECTED":
+	//       return "warning";
+	//     default:
+	//       break;
+	//   }
+	// }
+	// function getTextStatus(status: any) {
+	//   switch (status) {
+	//     case "PENDING":
+	//       return "En_attente";
+	//     case "APPROVED":
+	//       return "Validé";
+	//     case "REJECTED":
+	//       return "Refusé";
+	//     default:
+	//       break;
+	//   }
+	// }
 
-  const handleAdd = () => {
-    // dispatch(cancelEdit());
-  };
+	const handleAdd = () => {
+		// dispatch(cancelEdit());
+	};
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - listGrants.length) : 0;
+	// Avoid a layout jump when reaching the last page with empty rows.
+	const emptyRows =
+		page > 0 ? Math.max(0, (1 + page) * rowsPerPage - listGrants.length) : 0;
 
-  return (
-    <Container maxWidth="xl">
-      <SectionNavigation direction="row" justifyContent="space-between" mb={2}>
-        <Link href="/configurations/grant/add">
-          <Button
-            onClick={handleAdd}
-            variant="contained"
-            size="small"
-            startIcon={<Add />}
-          >
-            Créer
-          </Button>
-        </Link>
-        <Typography variant="h4">GRANTS</Typography>
-      </SectionNavigation>
-      <SectionTable>
-        <Box sx={{ width: "100%" }}>
-          <Paper sx={{ width: "100%", mb: 2 }}>
-            <GrantTableToolbar />
-            <TableContainer>
-              <Table
-                sx={{ minWidth: 750 }}
-                aria-labelledby="tableTitle"
-                size="small"
-              >
-                <GrantTableHead />
-                <TableBody>
-                  {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+	return (
+		<Container maxWidth="xl">
+			<Stack>
+				<SectionNavigation
+					direction="row"
+					justifyContent="space-between"
+					mb={2}
+				>
+					<Link href="/configurations/grant/add">
+						<Button
+							onClick={handleAdd}
+							variant="contained"
+							size="small"
+							startIcon={<Add />}
+						>
+							Créer
+						</Button>
+					</Link>
+					<Typography variant="h4">GRANTS</Typography>
+				</SectionNavigation>
+				<Divider />
+			</Stack>
+			<SectionTable>
+				<Box sx={{ width: "100%" }}>
+					<Paper sx={{ width: "100%", mb: 2 }}>
+						<GrantTableToolbar />
+						<TableContainer>
+							<Table
+								sx={{ minWidth: 750 }}
+								aria-labelledby="tableTitle"
+								size="small"
+							>
+								<GrantTableHead />
+								<TableBody>
+									{/* if you don't need to support IE11, you can replace the `stableSort` call with:
               rows.slice().sort(getComparator(order, orderBy)) */}
-                  {listGrants
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row: any, index: any) => {
-                      const labelId = `enhanced-table-checkbox-${index}`;
+									{listGrants
+										.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+										.map((row: any, index: any) => {
+											const labelId = `enhanced-table-checkbox-${index}`;
 
-                      return (
-                        <TableRow hover tabIndex={-1} key={row.id}>
-                          <TableCell
-                            component="th"
-                            id={labelId}
-                            scope="row"
-                            padding="normal"
-                            align="left"
-                          >
-                            {row.code_grant}
-                          </TableCell>
-                          <TableCell
-                            component="th"
-                            id={labelId}
-                            scope="row"
-                            padding="normal"
-                            align="left"
-                          >
-                            {row?.nom}
-                          </TableCell>
-                          <TableCell
-                            component="th"
-                            id={labelId}
-                            scope="row"
-                            padding="normal"
-                            align="left"
-                          >
-                            {row.post_analytique}
-                          </TableCell>
+											return (
+												<TableRow hover tabIndex={-1} key={row.id}>
+													<TableCell
+														component="th"
+														id={labelId}
+														scope="row"
+														padding="normal"
+														align="left"
+													>
+														{row.code_grant}
+													</TableCell>
+													<TableCell
+														component="th"
+														id={labelId}
+														scope="row"
+														padding="normal"
+														align="left"
+													>
+														{row?.nom}
+													</TableCell>
+													<TableCell
+														component="th"
+														id={labelId}
+														scope="row"
+														padding="normal"
+														align="left"
+													>
+														{row.post_analytique}
+													</TableCell>
 
-                          <TableCell align="right">
-                            <BtnActionContainer
-                              direction="row"
-                              justifyContent="right"
-                            >
-                              <Button
-                                variant="outlined"
-                                color="accent"
-                                // onClick={handleOpen}
-                                startIcon={<Add />}
-                                size="small"
-                              >
-                                Ligne budgetaire
-                              </Button>
+													<TableCell align="right">
+														<BtnActionContainer
+															direction="row"
+															justifyContent="right"
+														>
+															<Button
+																variant="outlined"
+																color="accent"
+																// onClick={handleOpen}
+																startIcon={<Add />}
+																size="small"
+															>
+																Ligne budgetaire
+															</Button>
 
-                              <Link
-                                href={`/configurations/grant/${row.id}/edit`}
-                              >
-                                <IconButton
-                                  color="primary"
-                                  aria-label="Modifier"
-                                  size="small"
-                                  component="span"
-                                >
-                                  <EditIcon />
-                                </IconButton>
-                              </Link>
-                              <IconButton
-                                color="warning"
-                                aria-label="Supprimer"
-                                size="small"
-                                component="span"
-                                onClick={() => {
-                                  handleclickDelete(row.id);
-                                }}
-                              >
-                                <DeleteIcon />
-                              </IconButton>
-                            </BtnActionContainer>
-                          </TableCell>
-                        </TableRow>
-                      );
-                    })}
-                  {emptyRows > 0 && (
-                    <TableRow
-                      style={{
-                        height: (dense ? 33 : 53) * emptyRows,
-                      }}
-                    >
-                      <TableCell colSpan={6} />
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={listGrants.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              labelDisplayedRows={defaultLabelDisplayedRows}
-              labelRowsPerPage={labelRowsPerPage}
-            />
-          </Paper>
-          {/* <FormControlLabel
+															<Link
+																href={`/configurations/grant/${row.id}/edit`}
+															>
+																<IconButton
+																	color="primary"
+																	aria-label="Modifier"
+																	size="small"
+																	component="span"
+																>
+																	<EditIcon />
+																</IconButton>
+															</Link>
+															<IconButton
+																color="warning"
+																aria-label="Supprimer"
+																size="small"
+																component="span"
+																onClick={() => {
+																	handleclickDelete(row.id);
+																}}
+															>
+																<DeleteIcon />
+															</IconButton>
+														</BtnActionContainer>
+													</TableCell>
+												</TableRow>
+											);
+										})}
+									{emptyRows > 0 && (
+										<TableRow
+											style={{
+												height: (dense ? 33 : 53) * emptyRows,
+											}}
+										>
+											<TableCell colSpan={6} />
+										</TableRow>
+									)}
+								</TableBody>
+							</Table>
+						</TableContainer>
+						<TablePagination
+							rowsPerPageOptions={[5, 10, 25]}
+							component="div"
+							count={listGrants.length}
+							rowsPerPage={rowsPerPage}
+							page={page}
+							onPageChange={handleChangePage}
+							onRowsPerPageChange={handleChangeRowsPerPage}
+							labelDisplayedRows={defaultLabelDisplayedRows}
+							labelRowsPerPage={labelRowsPerPage}
+						/>
+					</Paper>
+					{/* <FormControlLabel
         control={<Switch checked={dense} onChange={handleChangeDense} />}
         label="Dense padding"
       /> */}
-        </Box>
-      </SectionTable>
-    </Container>
-  );
+				</Box>
+			</SectionTable>
+		</Container>
+	);
 };
 
 export default ListGrant;
 
 export const BtnActionContainer = styled(Stack)(({ theme }) => ({}));
-export const SectionNavigation = styled(Stack)(({ theme }) => ({}));
-const SectionTable = styled("div")(({ theme }) => ({}));
+export const SectionNavigation = styled(Stack)(({ theme }) => ({
+	marginBottom: theme.spacing(1),
+}));
+const SectionTable = styled("div")(({ theme }) => ({
+	marginTop: theme.spacing(1),
+}));
--- pages/configurations/grant/index.tsx
+++ pages/configurations/grant/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Container from "@mui/material/Container";
 import BackOfficeLayout from "../../../layouts/backOffice";
 import ListGrant from "../../../components/configurations/grant/ListGrant";
 
 const index = () => {
-  return (
-    <BackOfficeLayout>
-      <ListGrant />
-    </BackOfficeLayout>
-  );
+	return (
+		<BackOfficeLayout>
+			<Container maxWidth="xl">
+				<ListGrant />
+			</Container>
+		</BackOfficeLayout>
+	);
 };
 
 export default index;
