1ee34f33d4e75f78944621f85b89de43493b8079
ogino89 <soandroniriela@gmail.com>
2022-10-07 05:06:33 +0300

    Feat/integration-post-analytique

---
+++ components/configurations/post-analytique/hooks/useFetchPostAnalytique.ts
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+// import { getAuxiliairyAccountList } from "../../../../redux/features/auxiliairyAccount";
+
+/**
+ * @description Hook to fetch grants
+ */
+const useFetchPostAnalytique = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  return async () => {
+    let args: any = {};
+    if (router.query.search) {
+      args.where = {};
+    }
+    if (router.query.orderBy && router.query.order) {
+      args.orderBy = {
+        [<string>router.query.orderBy]: router.query.order,
+      };
+    }
+    // await dispatch(getAuxiliairyAccountList({ args }));
+  };
+};
+
+export default useFetchPostAnalytique;
